dfc232943eb23ec9623676916c9fbab86301f83a
Ravi Chimmalgi <ravi.chimmalgi@gmail.com>
2016-04-07 17:21:49 -0500

    fix: comparing object to int

--- maze/Maze.java
+++ maze/Maze.java
@@ -6,8 +6,8 @@
  *  Generates a perfect N-by-N maze using depth-first search with a stack.
  *  Note: this program generalizes nicely to finding a random tree
  *        in a graph.
- *  Copyright © 2000–2011, Robert Sedgewick and Kevin Wayne. 
- * 
+ *  Copyright © 2000–2011, Robert Sedgewick and Kevin Wayne.
+ *
  *  Copyright © 2013, Ravi Chimmalgi for Random DFS (SolveRandom methods)
  *************************************************************************/
 package maze;
@@ -21,9 +21,9 @@ public class Maze {
     private boolean[][] south;
     private boolean[][] west;
     private boolean[][] visited;
-    
+
     private boolean done = false;
-   
+
 
     public Maze(int N) {
         this.N = N;
@@ -38,9 +38,9 @@ public class Maze {
         visited = new boolean[N+2][N+2];
         for (int x = 0; x < N+2; x++) visited[x][0] = visited[x][N+1] = true;
         for (int y = 0; y < N+2; y++) visited[0][y] = visited[N+1][y] = true;
-        
 
-       
+
+
 
         // initialze all walls as present
         north = new boolean[N+2][N+2];
@@ -51,22 +51,22 @@ public class Maze {
             for (int y = 0; y < N+2; y++)
                 north[x][y] = east[x][y] = south[x][y] = west[x][y] = true;
     }
-   
 
-    
+
+
 //Consider maze as a N X N matrix of cells, then the following methods return true if
-//there is wall to the top, right, left or bottom to the cell. 0 <= x,y < N    
+//there is wall to the top, right, left or bottom to the cell. 0 <= x,y < N
 public boolean hasTopWall(int x, int y){
-    return south[y+1][x+1]; 
+    return south[y+1][x+1];
  }
 public boolean hasRightWall(int x, int y){
-    return east[y+1][x+1]; 
+    return east[y+1][x+1];
  }
 public boolean hasLeftWall(int x, int y){
-    return west[y+1][x+1]; 
+    return west[y+1][x+1];
  }
 public boolean hasBottomWall(int x, int y){
-    return north[y+1][x+1]; 
+    return north[y+1][x+1];
  }
 
 
@@ -111,10 +111,10 @@ public boolean hasBottomWall(int x, int y){
         generate(1, 1);
 
 
-     
+
     }
 
-    
+
        // solve the maze starting from the start state
     public void solveDFS() {
         for (int x = 1; x <= N; x++)
@@ -140,16 +140,16 @@ public boolean hasBottomWall(int x, int y){
         if (x == N && y == N) done = true;
 
         if (!north[x][y]) {
-           // System.out.println("South"); 
+           // System.out.println("South");
         solveDFS(x, y + 1);}
         if (!east[x][y])  {
-            //System.out.println("East"); 
+            //System.out.println("East");
             solveDFS(x + 1, y);}
         if (!south[x][y]) {
-            //System.out.println("North"); 
+            //System.out.println("North");
             solveDFS(x, y - 1);}
         if (!west[x][y])  {
-            //System.out.println("West"); 
+            //System.out.println("West");
             solveDFS(x - 1, y);}
 
         if (done) return;
@@ -160,7 +160,7 @@ public boolean hasBottomWall(int x, int y){
         StdDraw.show(30);
     }
 
- 
+
 
         public void solveRandom() {
              StdDraw.setPenColor(StdDraw.BLUE);
@@ -170,47 +170,47 @@ public boolean hasBottomWall(int x, int y){
         done = false;
         solveRandom(1, 1);
     }
-        
+
      public class CellCoordinate {
     int x;
     int y;
-    
+
      CellCoordinate(int x, int y){
       this.x=x;
       this.y=y;
     }
 }
 
-    
+
     private Stack<CellCoordinate> decisionPoints = new Stack<>();
-    
+
     public void solveRandom(int x, int y){
         if (x == 0 || y == 0 || x == N+1 || y == N+1) return;
         if (done ) return;
         visited[x][y] = true;
-        
-       
+
+
         StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
         StdDraw.show(30);
         StdDraw.setPenColor(StdDraw.BLUE);
         if (x == N && y == N) done = true;
-        
-        
+
+
         ArrayList possiblePaths = new ArrayList<>();
-                
+
                 if(!north[x][y] & !visited[x][y+1]) possiblePaths.add(1);
                 if(!east[x][y] & !visited[x+1][y]) possiblePaths.add(2);
                 if(!south[x][y] & !visited[x][y-1]) possiblePaths.add(3);
                 if(!west[x][y] & !visited[x-1][y]) possiblePaths.add(4);
                 //System.out.println("paths: "+possiblePaths.size());
-                
-                
-                if(possiblePaths.size()>1) 
+
+
+                if(possiblePaths.size()>1)
                 {CellCoordinate cell = new CellCoordinate(x,y);
                 //System.out.println("decisionPoint: "+x+" "+y+" Paths: "+possiblePaths.size());
                 decisionPoints.push(cell);
                 }
-               
+
                if(possiblePaths.isEmpty())
                {
                     CellCoordinate c1;
@@ -223,22 +223,22 @@ public boolean hasBottomWall(int x, int y){
                     //System.out.println("peek "+c2.x+" "+c2.y);
                     StdDraw.setPenColor(StdDraw.BLACK);
                     solveRandom(c1.x,c1.y);}
-               } 
+               }
                else {
                 Random random = new Random();
                int index = random.nextInt(possiblePaths.size());
                //System.out.println(possiblePaths.get(index));
-               if(possiblePaths.get(index)==1)
-                   {//System.out.println("South"); 
+               if(possiblePaths.get(index).equals(1))
+                   {//System.out.println("South");
                    solveRandom(x, y + 1);}
-               if(possiblePaths.get(index)==2)
-                   {//System.out.println("East"); 
+               if(possiblePaths.get(index).equals(2))
+                   {//System.out.println("East");
                    solveRandom(x + 1, y);}
-               if(possiblePaths.get(index)==3)
-                   {//System.out.println("North"); 
+               if(possiblePaths.get(index).equals(3))
+                   {//System.out.println("North");
                    solveRandom(x, y - 1);}
-               if(possiblePaths.get(index)==4)
-                   {//System.out.println("West"); 
+               if(possiblePaths.get(index).equals(4))
+                   {//System.out.println("West");
                    solveRandom(x - 1, y);}
                }
                 if (done) return;
@@ -248,17 +248,17 @@ public boolean hasBottomWall(int x, int y){
         StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
         StdDraw.show(30);
     }
-    
-    
-    
+
+
+
     // draw the maze
     public void draw() {
         StdDraw.setPenColor(StdDraw.RED);
-        
+
         //Edit: Changed starting and ending points
         StdDraw.filledCircle(1.5, 1.5, 0.375);
         StdDraw.filledCircle(N + 0.5, N + 0.5, 0.375);
-        
+
 
         StdDraw.setPenColor(StdDraw.BLACK);
         for (int x = 1; x <= N; x++) {
@@ -271,15 +271,15 @@ public boolean hasBottomWall(int x, int y){
         }
         StdDraw.show(10);
     }
-    
+
     public void drawWallMatrix(){
-        
+
         System.out.println("Top walls");
         for(int x=1;x<N+1;x++)
         { for(int y=1;y<N+1;y++)
         {   if(south[y][x]==true)
             System.out.print(1+" ");
-        else 
+        else
             System.out.print(0+" ");
         }
         System.out.println();
@@ -290,7 +290,7 @@ public boolean hasBottomWall(int x, int y){
         { for(int y=1;y<N+1;y++)
         {   if(east[y][x])
             System.out.print(1+" ");
-        else 
+        else
             System.out.print(0+" ");
         }
         System.out.println();
@@ -301,12 +301,12 @@ public boolean hasBottomWall(int x, int y){
         { for(int y=1;y<N+1;y++)
         {   if(west[y][x])
             System.out.print(1+" ");
-        else 
+        else
             System.out.print(0+" ");
         }
         System.out.println();
     }
-        
+
           System.out.println();
             System.out.println("Bottom walls");
         for(int x=1;x<N+1;x++)
@@ -317,15 +317,15 @@ public boolean hasBottomWall(int x, int y){
         }
         System.out.println();
     }
-        
-        
-        
+
+
+
     }
 
 
     // a test client
     public static void main(String[] args) {
-       
+
         long startTime = 0;
         long endTime = 0;
         double diff = 0;
@@ -336,7 +336,7 @@ public boolean hasBottomWall(int x, int y){
             System.out.println(i);
             for (int j = 0; j < numRuns; j++) {
                 Maze maze = new Maze(i);
-              
+
                 StdDraw.clear();
                 StdDraw.show(0);
                 maze.draw();
@@ -344,28 +344,28 @@ public boolean hasBottomWall(int x, int y){
                 maze.solveDFS();
                 endTime = System.nanoTime();
                 diff = (endTime - startTime);
-          
+
                  System.out.println("   " + diff);
             }
-          
+
         }
-        
-//        
+
+//
 //        int N = 50;
 //        Maze maze = new Maze(N);
 //       // maze.initMatrix();
 //        StdDraw.show(0);
-//        
+//
 //        maze.draw();
 //        maze.solveRandom();
 //        //maze.solveDFS();
-//       
+//
 //       maze.drawWallMatrix();
 //       System.out.println(maze.hasTopWall(3, 2));
 //       System.out.println(maze.hasBottomWall(3, 2));
 //       System.out.println(maze.hasRightWall(3, 2));
 //       System.out.println(maze.hasLeftWall(3, 2));
-       
-    }   
+
+    }
 
 }
